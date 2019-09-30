c552ecad5baad5fec169db8fbaf5ea99e6aff732
dipak <dipak@milaap.org>
2019-09-30 21:17:17 +0530

    Updated Travis CI to production

--- updateProduction.sh
+++ updateProduction.sh
@@ -4,15 +4,15 @@ echo "\n Pushing build output to production \n"
 # git config --global user.name "involvevtk"
 
 cd ..
-mkdir production_repo
-cd production_repo
+mkdir involvevtk.github.io
+cd involvevtk.github.io
 git init
 git config user.email "involvevtk@gmail.com"
 git config user.name "involvevtk"
-git pull https://$guser:$gpassword@github.com/$guser/test_travis_build.git
+git pull https://$guser:$gpassword@github.com/$guser/involvevtk.github.io.git
 cp -r /home/travis/build/involvevtk/web-app/public/* ./
 git add .
 git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
-git push https://$guser:$gpassword@github.com/$guser/test_travis_build.git master
+git push https://$guser:$gpassword@github.com/$guser/involvevtk.github.io.git master
 
 echo "\n Push updated code to production_repo \n"
