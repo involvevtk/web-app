50de3bf3d8e18162f1ef03d3676ca63d8d8a23c4
dipak <dipak@milaap.org>
2019-09-30 14:57:57 +0530

    Removed logs

--- updateProduction.sh
+++ updateProduction.sh
@@ -1,40 +1,21 @@
 echo "Pushing build output to production"
 
-
-setup_git() {
-  git config --global user.email "involvevtk@gmail.com"
-  git config --global user.name "involvevtk"
-}
+git config --global user.email "involvevtk@gmail.com"
+git config --global user.name "involvevtk"
 
 cd ..
-echo "Change direcotry path"
-
 mkdir production_repo
 cd production_repo
-
-
 git clone https://github.com/involvevtk/test_travis_build.git
-echo "clone test_travis_build repository"
-
-cd test_travis_build
-echo "changed direcotry to test_travis_build"
-
+cd test_travis_build/
 cp -r /home/travis/build/involvevtk/web-app/public/* ./
-echo "copied build public directory to repository"
-ls
-
-setup_git
-echo "Updated git config"
-
 git add .
-echo "Added all files"
-
 git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
-echo "Added commit message"
+
 
 git push origin master << EOF
 $guser
 $gpassword
 EOF
 
-echo "Push updated code to github"
+echo "Push updated code to production_repo"
