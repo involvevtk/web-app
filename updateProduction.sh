b32d0c24d0a0752c4b3d98767c6454909480e8f7
dipak <dipak@milaap.org>
2019-09-30 21:04:05 +0530

    Updated script with new structure

--- updateProduction.sh
+++ updateProduction.sh
@@ -6,13 +6,13 @@ echo "\n Pushing build output to production \n"
 cd ..
 mkdir production_repo
 cd production_repo
-git clone https://github.com/involvevtk/test_travis_build.git
-cd test_travis_build/
+git init
+git config user.email "involvevtk@gmail.com"
+git config user.name "involvevtk"
+git pull https://involvevtk:involve_123@github.com/involvevtk/test_travis_build.git
 cp -r /home/travis/build/involvevtk/web-app/public/* ./
 git add .
 git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
-
-
-git push git@github.com:involvevtk/test_travis_build.git master
+git push https://involvevtk:involve_123@github.com/involvevtk/test_travis_build.git master
 
 echo "\n Push updated code to production_repo \n"
