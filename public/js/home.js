307eb6e52531803fb1437f30646292f1a59c90dd
dipak <dipak@milaap.org>
2019-10-15 22:16:29 +0530

    Cache slider images for faster loading
     - Introduced cloudinary storage for storing images
     - Updated home page slider image urls
     - Removed slider image assets from resources
     - Updates slider images with new image styles
    
     TODO: Update slider image illustration

--- public/js/home.js
+++ public/js/home.js
@@ -3,5 +3,7 @@
 document.addEventListener("DOMContentLoaded", function () {
   console.log("Page loaded");
   var elems = document.querySelectorAll(".slider");
-  var instances = M.Slider.init(elems);
+  var instances = M.Slider.init(elems, {
+    height: 552
+  });
 });
